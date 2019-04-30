459de78a51d4936e53ef17881860103f81ba529e
Joshua Smith <30632852+Josmi27@users.noreply.github.com>
2019-04-29 20:42:07 -0700

    Update Basic_Client.py

--- Basic_Client.py
+++ Basic_Client.py
@@ -1,24 +1,34 @@
 import socket                   # Import socket module
 
 s = socket.socket()             # Create a socket object
-host = "fdd6:59a:341:0:2a7d:2868:1a7:ecc9"  #Ip address that the TCPServer  is there
-port = 50000                     # Reserve a port for your service every new transfer wants a new port or you must wait.
 
-s.connect((host, port))
-s.send("Hello server!")
+def main(serverAddr, serverPort):
+    s.connect((serverAddr, serverPort))
+    s.send("Hello server!")
 
-with open('received_file', 'wb') as f:
-    print('file opened')
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
+    with open('received_file', 'wb') as f:
+        print('file opened')
+        while True:
+            print('receiving data...')
+            data = s.recv(1024)
+            print('data=%s', (data))
+            if not data:
+                break
+            # write data to a file
+            f.write(data)
 
-f.close()
-print('Successfully get the file')
-s.close()
-print('connection closed')
+    f.close()
+    print('Successfully get the file')
+    s.close()
+    print('connection closed')
+
+if __name__ == "__main__":
+    serverAddr = 'localhost'
+    serverPort = 8080
+    if len(sys.argv) > 1:
+        serverAddr = sys.argv[1]
+    if len(sys.argv) > 2:
+        serverPort = sys.argv[2]
+    main(serverAddr, serverPort)
+
+
